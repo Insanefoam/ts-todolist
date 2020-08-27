367034df6b9f6f3647942178ab6c18771bf06339
Valeriy <valeriy@Vlads-iMac.local>
2020-08-27 18:48:31 +0600

    add enter press handler to todo form

--- src/components/TodoForm.tsx
+++ src/components/TodoForm.tsx
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useState, ChangeEvent } from "react";
 
 const TodoForm: React.FC = () => {
+  const [value, setValue] = useState("");
+
+  const changeHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(event.target.value);
+  };
+
+  const keyPressHandler = (event: React.KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === "Enter") alert("enter");
+  };
+
   return (
     <div className="input-group mt-3 pl-5 pr-5">
       <input
         type="text"
         className="form-control"
         placeholder="Type new todo text..."
-        aria-label="Type new todo text..."
-        aria-describedby="button-addon2"
+        value={value}
+        onChange={changeHandler}
+        onKeyPress={keyPressHandler}
       />
       <div className="input-group-append">
         <button className="btn btn-outline-secondary" type="button">
